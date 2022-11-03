deb3919293e7fcd9dca8bcc5566101151f0f7e1a
merciof <merciofilho@gmail.com>
2022-11-03 16:27:27 -0300

    create Cid migration

---
+++ migrations/20221102222917-create-patient.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('patients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      cpf: {
+        type: Sequelize.STRING,
+      },
+      nome: {
+        type: Sequelize.STRING,
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('patients');
+  },
+};
