daba4996030ce874e39b7955626a94f717884548
merciof <merciofilho@gmail.com>
2022-11-04 18:21:15 -0300

    create migration to add column to patients table

---
+++ migrations/20221104205244-add-sex-column-to-patient.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('patients', 'sex_id', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    queryInterface.removeColumn('patients', 'sex_id');
+  },
+};
